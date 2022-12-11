34dda9e93a640ab1a1ac555ef9bbc981c4adf4a4
Fede04colo <Frafede4c@gmail.com>
2022-12-11 22:38:20 +0100

    grafica

---
+++ src/client/Main.java
@@ -0,0 +1,15 @@
+package client;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Main {
+
+    public static void main (String args [])
+    {
+        new primoFrame();
+    }
+}
