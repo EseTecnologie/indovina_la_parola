3d35efe343e93979da13644929b4076e0a93d79b
Sottocasa Michele <contact@michelesottocasa.tech>
2022-12-12 23:24:40 +0100

    Showing first two word char

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -61,7 +61,7 @@ public class ClientHandler implements Runnable {
             }
         }
 
-        write(output, "Parola da trovare --> ;" + FindWordManager.convertToAsterisks(wordToFind, ""));
+        write(output, "Parola da trovare --> ;" + FindWordManager.convertToAsterisks(wordToFind, wordToFind.substring(0,1)));
 
         while (true) {
             received = read();
@@ -220,4 +220,19 @@ class RankingManager{
         }
         return toReturn;
     }
+
+/*    static void bubbleSort(int[] arr){
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+}*/
 }
